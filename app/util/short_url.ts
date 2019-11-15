1d312710ab6580497af232ce6f72d3ac673c5064
lenconda <i@lenconda.top>
2019-11-15 10:46:17 +0800

    fix shorturl

--- app/util/short_url.ts
+++ app/util/short_url.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import qs from 'qs'
+import cheerio from 'cheerio'
 
 export const generateShortURL = async (longurl: string) => {
   const form = qs.stringify({ longurl })
   const { data } = await axios
-      .post('http://qvni.cn/t.cn/ajax.php',
+  .post('https://tool.chinaz.com/tools/dwz.aspx',
       form,
       { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
-  return data
+  const $ = cheerio.load(data)
+  return $('#shorturl').text()
 }
